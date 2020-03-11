f56c491ac51940c36ec6c06ad338f2de20673760
ALO <36780370+Osteribo@users.noreply.github.com>
2020-03-11 12:11:02 -0700

    another aws build test

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import jwt
 
 # pylint: disable=import-error
 from flask import Flask, jsonify, request, abort
-#3333
+#33333
 
 JWT_SECRET = os.environ.get('JWT_SECRET', 'abc123abc1234')
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
